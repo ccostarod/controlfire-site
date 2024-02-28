2533361063d419adaf6a37733d4ab79bf722d825
ccostarod <ccostarodrigoo@gmail.com>
2024-02-28 16:08:09 -0300

    Criação da section clientes juntamente com swiper

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ function menuShow(){
         document.querySelector('.icon').src = "img/close_white_36dp.svg"
     }
 }
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
